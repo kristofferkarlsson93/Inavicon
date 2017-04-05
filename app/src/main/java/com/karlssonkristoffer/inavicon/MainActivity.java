485461e2d58f0ace3c8ceb848163afb5e5e686e1
Kristoffer Karlsson <kristoffer.karlsson93@hotmail.com>
2017-04-05 16:15:19 +0200

    Now we can chagne image depending on location. Example images

--- app/src/main/java/com/karlssonkristoffer/inavicon/MainActivity.java
+++ app/src/main/java/com/karlssonkristoffer/inavicon/MainActivity.java
@@ -1,14 +1,56 @@
 package com.karlssonkristoffer.inavicon;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
+    private MainListener mainListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainListener = new MainListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        mainListener.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mainListener.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //for debug
+    public void updatePosText(double latittude, double longitude) {
+        TextView text = (TextView) findViewById(R.id.location);
+        text.setText(String.valueOf(latittude) + "\n " + String.valueOf(longitude));
+    }
+    //for debug
+    public void updateGeofence(String name) {
+        TextView text = (TextView) findViewById(R.id.geofence);
+        text.setText("Entered: " + name);
+        ImageView image = (ImageView) findViewById(R.id.pingvin);
+        image.setImageResource(R.drawable.testimagetwo);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mainListener.onDestroy();
     }
 }
 
---
+++ app/src/main/java/com/karlssonkristoffer/inavicon/MainListener.java
@@ -0,0 +1,66 @@
+package com.karlssonkristoffer.inavicon;
+
+/**
+ * Created by Kristoffer on 2017-04-05.
+ */
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import io.proximi.proximiiolibrary.ProximiioAPI;
+import io.proximi.proximiiolibrary.ProximiioGeofence;
+import io.proximi.proximiiolibrary.ProximiioListener;
+
+
+public class MainListener extends ProximiioListener {
+    private ProximiioAPI proximiioAPI;
+    @Nullable private MainActivity main;
+    private static final String ID = "MainActivityListener";
+    private static final String AUTH_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImlzcyI6IjBkMTMyNGRmLTM0ZjctNDhkNS1hNzQ4LTEwOGU0ZmExMjkxNiIsInR5cGUiOiJhcHBsaWNhdGlvbiIsImFwcGxpY2F0aW9uX2lkIjoiOGZiZjE5YzUtZTAyMi00N2ExLWE2MDYtNGUzZjdhN2UzMDA3In0.6d6mfN4r0bBssavMbOxHlOzLKBnimWS0laCn7xNFgpw"; // TODO: Replace with your own!
+
+    public MainListener(@NonNull MainActivity main) {
+        this.main = main;
+        proximiioAPI = new ProximiioAPI(ID, main);
+        proximiioAPI.setActivity(main);
+        proximiioAPI.setListener(this);
+        proximiioAPI.setAuth(AUTH_KEY);
+        Log.d(ID, "Constructor");
+    }
+
+    void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        proximiioAPI.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    void onActivityResult(int requestCode, int resultCode, Intent data) {
+        proximiioAPI.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void position(double lat, double lon, double accuracy) {
+        // Do something with the positioning system.
+        // See ProximiioListener for all methods that can be overridden.
+        if (main != null) {
+            //main.someAction();
+            main.updatePosText(lat, lon);
+        }
+        Log.d(ID, "Position update! (" + lat + ", " + lon + ")");
+    }
+
+    @Override
+    public void geofenceEnter(ProximiioGeofence geofence) {
+        Log.d(ID, "Entered geofence: " + geofence.getName());
+        main.updateGeofence(geofence.getName());
+    }
+
+    void onDestroy() {
+        // Set to null to avoid memory leaks.
+        main = null;
+        // Destroy this API object.
+        // Parameter true indicates that we'd like to keep our listener working on the background.
+        proximiioAPI.destroy(true);
+    }
+
+}
+
